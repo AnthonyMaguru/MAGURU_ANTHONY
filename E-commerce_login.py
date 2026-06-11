760a63519157a8f18f36c5ea1b53126726650c8e
AnthonyMaguru <maguruanthony20@gmail.com>
2026-06-11 13:56:27 +0300

    login  system

---
+++ E-commerce_login.py
@@ -0,0 +1,21 @@
+print("===== E-COMMERCE LOGIN SYSTEM =====")
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "admin":
+    if password == "admin123":
+        print("Welcome Admin")
+        print("Access: Full System Access")
+    else:
+        print("Incorrect Password")
+
+elif username == "customer":
+    if password == "cust123":
+        print("Welcome Customer")
+        print("Access: Shopping Features")
+    else:
+        print("Incorrect Password")
+
+else:
+    print("User does not exist")
