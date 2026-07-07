58350e2428571c402fc903521ff7254f7e16a48b
AnthonyMaguru <maguruanthony20@gmail.com>
2026-07-07 23:46:40 +0300

    day13 exerxises and assignments

--- day12/scitkit.py
+++ day12/scitkit.py
@@ -6,4 +6,17 @@
 # 1. Data preprocessing - This involves data splitting, Feature scaling, Feature selection, Feature extraction.
 # Data Splitting - training and testing data
 # Feature scaling - Standardization, Normalization
+# absolute Maximum scaling: it is sensitive to outliers and best for clean data
+
+# 2. Model exvaluation
+# 3. pipeline support
+# 4. supports integration with other libraries like numpy, pandas, matplotlib, and seaborn
+# 5. easy to use
+
+# installing scikit library
+# pip install scikit-learn
+
+# Machine learning techniques supported
+# under supervised learning - Classification e.g logistic regression, Decision tree, Random forest, Support vector machine, K-nearest neighbor
+
 
