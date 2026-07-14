24ffd37af8acf398e89ea37935d401ef320e90e3
AnthonyMaguru <maguruanthony20@gmail.com>
2026-07-14 18:57:51 +0300

    Student information system

---
+++ day15/Django.py
@@ -0,0 +1,16 @@
+
+
+# - Is an isolated workspace that contains it's own python interpreter and installed packages.
+
+#Activate virtual environment
+
+#source .venv/Scripts/activate
+
+#MVT which stands for Model-View-Template design style
+#1 - Model: represents the data you want to display
+#2 -View: Handles incoming requests and return the appropriate
+#3 -Template: A text file (usually HTML) that defines the structure or layout of a file (or a webpage)
+
+#how to create a Django project
+# use django-admin startproject project_name
+
